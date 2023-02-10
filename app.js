0e3498727d722b4f8220521dbe35bad726f6e24f
NingDin <114199756+NingDin@users.noreply.github.com>
2023-02-09 20:04:20 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //1.Use the D3 library to read in samples.json from the URL https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json.
-const url='https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json'
+const url="samples.json"
+      //'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json'
 const dataPromise=d3.json(url);
 console.log(`Data Promise:${dataPromise}`);
 let datajson=d3.json(url).then(function(x){console.log(x);});
